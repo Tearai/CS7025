fdcdcb7222c80131adef3b652b7bc066b443aee2
Tearai <77899947+Tearai@users.noreply.github.com>
2024-11-06 17:25:47 +0000

    Variables, functions and operators Lab

---
+++ Variables_Function_Operators_Lab/ConsoleLog.js
@@ -0,0 +1,28 @@
+function sum(a, b)
+{
+    return a + b;
+}
+
+console.log(sum(8, 21));
+
+
+var run = false;
+
+for (let i = 0; i < 11; i++)
+    {
+        if(i <11)
+        {
+            run = true;
+
+            if(run == true)
+            {
+                console.log("you ran!");
+            }
+        }
+        
+    }
+
+
+
+
+    

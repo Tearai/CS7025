d7386dcbdb2cdee62d9a784795c4a2fd7375153e
Tearai <77899947+Tearai@users.noreply.github.com>
2024-11-06 17:25:22 +0000

    Condition and Loops Lab

---
+++ Condition_Loops_Lab/lab.js
@@ -0,0 +1,19 @@
+for (let time = 0; time < 100; time++)
+{
+    //console.log(time);
+
+    if(time == 50)
+    {
+        console.log("it's morning");
+    }
+
+    while(time > 50)
+    {
+        if(time < 100)
+        {
+            console.log("wake up!")
+            break;
+        }
+        
+    }
+}   
